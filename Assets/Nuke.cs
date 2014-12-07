86a4090bdd4554eb8e3add48f7d6585559fa0173
farrelNZL <lebecjohan@gmail.com>
2014-12-07 12:07:32 +0100

    prototypeTVExtinction

---
+++ Assets/Nuke.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nuke : MonoBehaviour {
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag== ("Player")) {
+			Destroy (this.gameObject);
+			other.GetComponent<Control2> ().addAmmo ();
+		}
+	}
+
+}
