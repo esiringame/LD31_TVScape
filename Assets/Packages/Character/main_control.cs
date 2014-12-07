23a19907a3a76a7d602b12955fb4baebd76f29e9
Karamaz0V1 <florent.guiotte@gmail.com>
2014-12-07 13:28:36 +0100

    Ajout du screenGlass

---
+++ Assets/Packages/Character/main_control.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main_control : MonoBehaviour {
+	public float maxSpeed = 10f;
+	bool facingRight = false;
+    public int jump_force = 700;
+    Animator anim;
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	void FixedUpdate()  {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("ground", grounded);
+
+        anim.SetFloat("vSpeed", Mathf.Abs(rigidbody2D.velocity.y));
+
+        float move = Input.GetAxis ("Horizontal");
+
+        anim.SetFloat("speed", Mathf.Abs(move));
+
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+        //rigidbody2D.velocity = new Vector2(jump * maxSpeed, rigidbody2D.velocity.x);
+
+        if (move > 0 && !facingRight)
+            Flip ();
+        else if (move < 0 && facingRight)
+            Flip ();
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(grounded && Input.GetKeyDown(KeyCode.Space)) {
+            anim.SetBool("Ground", false);
+            rigidbody2D.AddForce(new Vector2(0, jump_force));
+        }
+
+	
+	}
+}
