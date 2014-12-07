cf10ba2962918d79900caa8a2a967136929163d5
Karamaz0V1 <florent.guiotte@gmail.com>
2014-12-07 13:53:28 +0100

    Ajout du script de gestion de l'écran

---
+++ Assets/Scripts/Level/ScreenScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenScript : MonoBehaviour {
+
+    public GameObject glassScreen0;
+    public GameObject glassScreen1;
+    public GameObject glassScreen2;
+    public GameObject glassScreen3;
+    public GameObject blankScreen;
+
+	// Use this for initialization
+	void Start () {
+        glassScreen0.GetComponent<SpriteRenderer>().enabled = false;
+        glassScreen1.GetComponent<SpriteRenderer>().enabled = false;
+        glassScreen2.GetComponent<SpriteRenderer>().enabled = false;
+        glassScreen3.GetComponent<SpriteRenderer>().enabled = false;
+        blankScreen.GetComponent<SpriteRenderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
