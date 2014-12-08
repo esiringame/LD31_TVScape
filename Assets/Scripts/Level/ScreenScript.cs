2bdc45868d6a5903c98122da276e4bfb55414642
ReMinoer <rmorillon@freneticexperience.com>
2014-12-08 22:37:09 +0100

    Partial commit Hammer

--- Assets/Scripts/Level/ScreenScript.cs
+++ Assets/Scripts/Level/ScreenScript.cs
@@ -13,6 +13,17 @@ public class ScreenScript : MonoBehaviour {
     public bool explode = false;
     private bool waitActive = false;
 
+	public bool totallyExplode
+	{
+		get
+		{
+			return glassScreen0.GetComponent<SpriteRenderer>().enabled
+				&& glassScreen1.GetComponent<SpriteRenderer>().enabled
+				&& glassScreen2.GetComponent<SpriteRenderer>().enabled
+				&& glassScreen3.GetComponent<SpriteRenderer>().enabled;
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
         glassScreen0.GetComponent<SpriteRenderer>().enabled = false;
@@ -35,7 +46,7 @@ public class ScreenScript : MonoBehaviour {
             blankScreen.GetComponent<SpriteRenderer>().enabled = false;
 	}
 
-    void Explode (int screen) {
+    public void Explode (int screen) {
         StartCoroutine(Wait());
         particle.GetComponent<ParticleSystem>().Play();
         switch (screen) {
@@ -50,9 +61,23 @@ public class ScreenScript : MonoBehaviour {
         }
     }
 
-     IEnumerator Wait(){
-         waitActive = true;
-         yield return new WaitForSeconds (0.01f);
-         waitActive = false;
+	public GameObject GetGlass(int corner)
+	{
+		switch (corner) {
+		case 0 : return glassScreen0;
+			break;
+		case 1 : return glassScreen1;
+			break;
+		case 2 : return glassScreen2;
+			break;
+		case 3 : return glassScreen3;
+			break;
+		}
+	}
+	
+	IEnumerator Wait(){
+		waitActive = true;
+		yield return new WaitForSeconds (0.01f);
+		waitActive = false;
      }
 }
---
+++ Assets/SpawnHammer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnHammer : MonoBehaviour {
+
+	public GameObject prefab;
+	public GameObject screenScript;
+
+	private float deltaHammer = 0;
+	private float timerHammer = 20;
+
+	// Use this for initialization
+	void Start ()
+	{
+		deltaHammer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		deltaHammer += Time.deltaTime;
+		if (deltaHammer >= timerHammer)
+		{
+			GameObject glass;
+			int corner;
+			do
+			{
+				corner = Random.Range(0,4);
+				glass = screenScript.GetComponent<ScreenScript>().GetGlass(corner);
+			} while (!glass.GetComponent<SpriteRenderer>().enabled);
+			/*
+			GameObject colored = Instantiate(prefab, new Vector3 (spawnPoint.x, spawnPoint.y, 0), Quaternion.identity) as GameObject;
+*/
+deltaHammer -= timerHammer;
+		}
+	}
+}
