c584e76b00398d93af2e5586a808384390635536
ReMinoer <rmorillon@freneticexperience.com>
2014-12-09 00:06:38 +0100

    Spawn & behaviour of hammers

--- Assets/HammerBehaviour.cs
+++ Assets/HammerBehaviour.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class HammerBehaviour : MonoBehaviour {
 
-	public GameObject player;
 	public GameObject screenScript;
-
+	public GameObject spawnHammer;
 	public int corner;
 
 	void OnTriggerEnter2D(Collider2D collider)
 	{
 		if (collider.gameObject.tag == "Player")
+		{
 			screenScript.GetComponent<ScreenScript>().Explode(corner);
+			spawnHammer.GetComponent<SpawnHammer>().stillHammer = false;
+			Destroy(gameObject);
+		}
 	}
 }
--- Assets/SpawnHammer.cs
+++ Assets/SpawnHammer.cs
@@ -6,6 +6,10 @@ public class SpawnHammer : MonoBehaviour {
 	public GameObject prefab;
 	public GameObject screenScript;
 
+	public bool stillHammer;
+	public int width;
+	public int height;
+
 	private float deltaHammer = 0;
 	private float timerHammer = 20;
 
@@ -13,11 +17,15 @@ public class SpawnHammer : MonoBehaviour {
 	void Start ()
 	{
 		deltaHammer = 0;
+		stillHammer = false;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		if (stillHammer)
+			return;
+
 		deltaHammer += Time.deltaTime;
 		if (deltaHammer >= timerHammer)
 		{
@@ -27,10 +35,29 @@ public class SpawnHammer : MonoBehaviour {
 			{
 				corner = Random.Range(0,4);
 				glass = screenScript.GetComponent<ScreenScript>().GetGlass(corner);
-			} while (!glass.GetComponent<SpriteRenderer>().enabled);
-			GameObject colored = Instantiate(prefab, new Vector3 (transform.position.x, transform.position.y, 0), Quaternion.identity) as GameObject;
+			} while (glass.GetComponent<SpriteRenderer>().enabled);
 
+			Vector2 spawnPosition = centerPortionSpawn(corner);
+			GameObject hammer = Instantiate(prefab, new Vector3 (spawnPosition.x, spawnPosition.y, 0), Quaternion.identity) as GameObject;
+			hammer.GetComponent<HammerBehaviour>().corner = corner;
+			hammer.GetComponent<HammerBehaviour>().screenScript = screenScript;
+			hammer.GetComponent<HammerBehaviour>().spawnHammer = gameObject;
+
+			stillHammer = true;
 			deltaHammer -= timerHammer;
 		}
+	}
+
+	Vector2 centerPortionSpawn (int corner)
+	{
+		Vector2 center = new Vector2(transform.position.x, transform.position.y);
+		switch (corner)
+		{
+			case 0: return new Vector2(center.x - width/2, center.y + height/2); break;
+			case 1: return new Vector2(center.x + width/2, center.y + height/2); break;
+			case 2: return new Vector2(center.x + width/2, center.y - height/2); break;
+			case 3: return new Vector2(center.x - width/2, center.y - height/2); break;
+		}
+		return Vector2.zero;
 	}
 }
